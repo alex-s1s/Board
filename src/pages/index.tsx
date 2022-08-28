9e15e8eea9a7384256c126ccf1feb589a3962fe1
Alex-da-Silva-Mendes <alex.da.silva.mendes@ibm.com>
2022-08-28 01:56:33 -0300

    feat: create navbar, home and board

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,22 @@ export default function Home() {
       <Head>
         <title>Board - Organizando suas tarefas.</title>
       </Head>
-      <div>
-        <h1 className={styles.title}>
-          Primeiro projeto com <span>next</span>
-        </h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <img src="/images/board-user.svg" alt="Ferramenta board" />
+
+        <section className={styles.callToAction}>
+          <h1>
+            Uma ferramenta para seu dia a dia Escrea, planeje e organize-se..
+          </h1>
+          <p>
+            <span>100% gratuita</span> e online.
+          </p>
+        </section>
+
+        <div className={styles.donators}>
+          <img src="https://sujeitoprogramador.com/steve.png" alt="usuário 1" />
+        </div>
+      </main>
     </>
   );
 }
